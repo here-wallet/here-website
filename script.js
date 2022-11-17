8ccc809646c86e75814e582fcd610feeba32a9f1
Andrey <azbangwtf@ya.ru>
2022-11-18 01:38:39 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const page = document.getElementsByClassName("page-background")[0]
 const header = document.getElementsByClassName("header")[0]
 
-document.addEventListener('scroll', (e) => {
-    let scroll = Math.max(0, this.scrollY - 820) * 0.005
-    let offset = Math.max(88 * (1 - scroll), 0);
+const updateScroll = () => {
+    let pad = window.innerWidth < 1600 ? 44 : 88 
+    pad = window.innerWidth < 1200 ? 12 : pad 
+
+    let tophold =  window.innerWidth < 800 ? 260 : 620
+    let scroll = Math.max(0, this.scrollY - tophold) * 0.005
+    let offset = Math.max(pad * (1 - scroll), 0);
     page.style.left = offset + 'px'
     page.style.right = offset + 'px'
 
     header.classList.toggle('active', this.scrollY > 0)
-})
+}
+
+document.addEventListener('scroll', updateScroll)
+updateScroll()
