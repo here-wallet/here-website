e1b940d185f3fdc4d0e0ea2dda02db6d293553b2
Valery <valerynistratov@gmail.com>
2023-05-07 02:49:25 +0500

    disable and hash valid

--- binance-waitlist/index.js
+++ binance-waitlist/index.js
@@ -88,7 +88,7 @@ window.addEventListener(
 
 function maskedUserName(inputValue) {
 	const value = inputValue.replace(/[^a-zA-Z0-9_.-]/g, '');
-	const isValid = value.length >= 2 && (/^[A-HJ-NP-Za-km-z1-9]*$/.test(value) || /^[0-9a-z]+.near/.test(value));
+	const isValid = value.length >= 2 && (/^[A-Fa-f0-9]+$/.test(value) || /^[0-9a-z]+.near/.test(value));
 	return { value, isValid };
 }
 // Form Logics
