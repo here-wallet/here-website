95913ba5ea14652b6f8df5e65cb151bbd3077480
Andrey <azbangwtf@ya.ru>
2023-07-06 13:48:15 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 scp -r ./howto/* root@web.herewallet.app:/var/www/here-www/howto
-scp -r ./stemmy/* root@web.herewallet.app:/var/www/here-www/stemmy
 scp -r ./privacypolicy/* root@web.herewallet.app:/var/www/here-www/privacypolicy
 scp -r ./nft/* root@web.herewallet.app:/var/www/here-www/nft
 scp -r ./dist/* root@web.herewallet.app:/var/www/here-www
