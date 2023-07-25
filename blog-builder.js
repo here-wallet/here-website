5d07a3fc9cf23ee3887fab488abb863dccbbea4f
Andrey <azbangwtf@ya.ru>
2023-07-26 00:46:24 +0300

    article builder

--- blog-builder.js
+++ blog-builder.js
@@ -1,113 +1,85 @@
-const fs = require('fs');
-const path = require('path');
-const mdFilesDir = './articles';
-const { Remarkable } = require('remarkable');
+const fs = require("fs");
+const path = require("path");
+const { Remarkable } = require("remarkable");
+const pug = require("pug");
 
-var md = new Remarkable();
-const pug = require('pug');
-const sass = require('sass')
+const md = new Remarkable();
+const pugConfig = JSON.parse(fs.readFileSync(".pugrc", "utf-8"));
+const mdFilesDir = "./articles";
 
-function compileArticle(mdHtml, mdata) {
-    const articleTemplate = './article.pug';
-    const pugConfig = JSON.parse(fs.readFileSync('.pugrc', 'utf-8'));
+async function compileArticle(mdHtml, mdata) {
+  const html = pug.renderFile("./blog/_article.pug", {
+    content: mdHtml,
+    metadata: mdata,
+    ...pugConfig.locals,
+  });
 
-    const html = pug.renderFile(articleTemplate, {
-        content: mdHtml,
-        metadata: mdata,
-        ...pugConfig.locals
-    });
-
-    const cssOutputPath = path.join('./dist/blog', 'style.css');
-    const outputPath = path.join('./dist/blog', mdata.href + '.html');
-
-
-    const sassFilePath = './blog/style.sass';
-    const compiledCss = sass.renderSync({ file: sassFilePath }).css;
-    fs.writeFileSync(cssOutputPath, compiledCss);
-
-    fs.writeFile(outputPath, html, (err) => {
-        if (err) {
-            console.error(`Error writing file: ${outputPath}`);
-        } else {
-            console.log(`File saved: ${outputPath}`);
-        }
-    });
+  await fs.promises.writeFile(`./blog/${mdata.href}.html`, html);
 }
 
 const generateHtml = async () => {
-    fs.readdirSync(mdFilesDir).map(async (file) => {
-        if (path.extname(file) === '.md') {
-            const mdFilePath = path.join(mdFilesDir, file);
-            const mdContent = await fs.promises.readFile(mdFilePath, 'utf-8');
+  const dir = fs.readdirSync(mdFilesDir);
+  for (const file of dir) {
+    if (path.extname(file) === ".md") {
+      const mdFilePath = path.join(mdFilesDir, file);
+      const mdContent = await fs.promises.readFile(mdFilePath, "utf-8");
 
-            const resultBlogPreview = extractMetadataFromMarkdown(mdContent);
-            const href = path.basename(mdFilePath, path.extname(mdFilePath));
-            resultBlogPreview.href = href;
+      const resultBlogPreview = extractMetadataFromMarkdown(mdContent);
+      const href = path.basename(mdFilePath, path.extname(mdFilePath));
+      resultBlogPreview.href = href;
 
-            const html = md.render(mdContent);
-            compileArticle(html, resultBlogPreview);
-            return html;
-        }
-        return null;
-    });
+      const html = md.render(mdContent);
+      await compileArticle(html, resultBlogPreview);
+    }
+  }
 };
 
-generateHtml();
-
-
-
-
-
-
-
-
-
-
-
-
 // previews builder for .pugrc
-
-
-
 const extractMetadataFromMarkdown = (markdown) => {
-    const charactersBetweenGroupedHyphens = /^-----([\s\S]*?)---/;
-    const metadataMatched = markdown.match(charactersBetweenGroupedHyphens);
-    const metadata = metadataMatched[1];
+  const charactersBetweenGroupedHyphens = /^-----([\s\S]*?)---/;
+  const metadataMatched = markdown.match(charactersBetweenGroupedHyphens);
+  const metadata = metadataMatched[1];
 
-    if (!metadata) {
-        return {};
-    }
+  if (!metadata) {
+    return {};
+  }
 
-    const metadataLines = metadata.split("\n");
-    const metadataObject = metadataLines.reduce((accumulator, line) => {
-        const [key, ...value] = line.split(":").map((part) => part.trim());
+  const metadataLines = metadata.split("\n");
+  const metadataObject = metadataLines.reduce((accumulator, line) => {
+    const [key, ...value] = line.split(":").map((part) => part.trim());
 
-        if (key)
-            accumulator[key] = value[1] ? value.join(":") : value.join("");
-        return accumulator;
-    }, {});
+    if (key) accumulator[key] = value[1] ? value.join(":") : value.join("");
+    return accumulator;
+  }, {});
 
-    return metadataObject;
+  return metadataObject;
 };
 
 const buildBlogPreviews = async () => {
-    const pug = JSON.parse(await fs.promises.readFile('.pugrc', 'utf-8'));
-    const blogPreviews = await Promise.all(
-        fs.readdirSync(mdFilesDir).map(async (file) => {
-            if (path.extname(file) === '.md') {
-                const mdFilePath = path.join(mdFilesDir, file);
-                const mdContent = await fs.promises.readFile(mdFilePath, 'utf-8');
-                const resultBlogPreview = extractMetadataFromMarkdown(mdContent);
-                resultBlogPreview.href = `${path.basename(mdFilePath, path.extname(mdFilePath))}`;
-                return resultBlogPreview;
-            }
-            return null;
-        })
-    );
+  const pug = JSON.parse(await fs.promises.readFile(".pugrc", "utf-8"));
+  const blogPreviews = await Promise.all(
+    fs.readdirSync(mdFilesDir).map(async (file) => {
+      if (path.extname(file) === ".md") {
+        const mdFilePath = path.join(mdFilesDir, file);
+        const mdContent = await fs.promises.readFile(mdFilePath, "utf-8");
+        const resultBlogPreview = extractMetadataFromMarkdown(mdContent);
+        resultBlogPreview.href = `${path.basename(
+          mdFilePath,
+          path.extname(mdFilePath)
+        )}`;
+        return resultBlogPreview;
+      }
+      return null;
+    })
+  );
 
-    const filteredBlogPreviews = blogPreviews.filter((article) => article !== null);
-    pug.locals.blogs = filteredBlogPreviews;
-    await fs.promises.writeFile('.pugrc', JSON.stringify(pug, null, 2));
+  const filteredBlogPreviews = blogPreviews.filter(
+    (article) => article !== null
+  );
+
+  pug.locals.blogs = filteredBlogPreviews;
+  await fs.promises.writeFile(".pugrc", JSON.stringify(pug, null, 2));
 };
 
 buildBlogPreviews();
+generateHtml();
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-scp -r ./howto/* fedor@web.herewallet.app:/var/www/here/www/howto
-scp -r ./privacypolicy/* fedor@web.herewallet.app:/var/www/here/www/privacypolicy
-scp -r ./nft/* fedor@web.herewallet.app:/var/www/here/www/nft
-scp -r ./dist/* fedor@web.herewallet.app:/var/www/here/www
-scp -r ./dist/landing/* fedor@web.herewallet.app:/var/www/here/www/landing
