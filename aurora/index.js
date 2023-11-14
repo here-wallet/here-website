ccb992854c7d0d98c0481ad48433c9dc25904afb
AZbang <azbangwtf@ya.ru>
2023-11-14 01:27:12 +0100

    fix aurora

--- aurora/index.js
+++ aurora/index.js
@@ -1,48 +1,103 @@
-const asks = Array.from(document.querySelectorAll(".section-faq__item"));
-asks.forEach((el) => {
-	el.addEventListener("click", () => {
-		const header = el.querySelector(".section-faq__ask");
-		const body = el.querySelector(".section-faq__answer");
-		const headerBox = header.getBoundingClientRect();
+import { HereWallet } from "@here-wallet/core";
+import { base_encode } from "near-api-js/lib/utils/serialize";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
-		asks.forEach((ask) => {
-			if (ask == el) return;
-			const header = ask.querySelector(".section-faq__ask");
-			const box = header.getBoundingClientRect();
-			ask.classList.remove("open");
-			ask.style.height = box.height + "px";
-		});
+const here = new HereWallet();
+const connectBtn = document.querySelector(".btn-connect-wallet");
 
-		el.style.height = headerBox.height + "px";
-		body.style.display = "block";
-		setTimeout(() => {
-			const { height } = body.getBoundingClientRect();
-			el.classList.toggle("open");
-			el.style.height = el.classList.contains("open")
-				? `${headerBox.height + height}px`
-				: headerBox.height + "px";
-		}, 10);
-	});
+const renderMission = (item) => {
+  const isDisabled = item.status === 0 ? "disabled" : "";
+  const isSuccess = item.status === 2 ? "success" : "";
+
+  return `<a  href="${item.link}" class="cart-rainbow cart-item ${isDisabled} ${isSuccess}">
+    <div class="cart-title">${item.title}</div>
+    <div class="cart-description">${item.description.replaceAll("\n", "<br />")}</div>
+    <div class="cart-transaction">
+      <p>${item.score_description}:</p>
+      <button>+${item.score}</button>
+    </div>
+    <img class="logo" src="${item.image_url}" />
+  </a>`;
+};
+
+const fetchUser = async () => {
+  connectBtn.innerHTML = "Connect wallet";
+
+  let url = `https://dev.herewallet.app/partners/aurora/missions`;
+  try {
+    const auth = JSON.parse(localStorage.getItem("account"));
+    const account = await here.account(auth.account_id);
+    connectBtn.innerHTML =
+      account.accountId.length > 16
+        ? account.accountId.slice(0, 8) + ".." + account.accountId.slice(-8)
+        : account.accountId;
+
+    url = `https://dev.herewallet.app/partners/aurora/missions?account_id=${auth.account_id}`;
+  } catch {}
+
+  const res = await fetch(url, { headers: { "Content-Type": "application/json" }, method: "GET" });
+  const data = await res.json();
+  console.log({ data });
+
+  const html = data.missions.map(renderMission).join("\n");
+  const list = document.querySelector(".cart-section");
+  list.innerHTML = html;
+};
+
+const register = async () => {
+  try {
+    let nonceArray = new Uint8Array(32);
+    const nonce = [...crypto.getRandomValues(nonceArray)];
+    const result = await here.signMessage({
+      receiver: "HERE Wallet",
+      message: "starbox",
+      nonce,
+    });
+
+    const auth = JSON.stringify({
+      account_id: result.accountId,
+      signature: base_encode(Buffer.from(result.signature)),
+      public_key: result.publicKey.toString(),
+      nonce,
+    });
+
+    localStorage.setItem("account", auth);
+    fetchUser();
+  } catch {
+    Toastify({ text: "Authorization failed", position: "center", className: "here-toast" }).showToast();
+  }
+};
+
+connectBtn.addEventListener("click", async () => {
+  localStorage.removeItem("account");
+  fetchUser();
+  register();
 });
 
-// Функция для выполнения асинхронного запроса к API
-    async function checkAuthentication() {
-      try {
-        // Выполняем запрос к API
-        const response = await fetch("https://api.herewallet.app/api/v1/docs#/partners_api/get_aurora_missions_partners_aurora_missions_get");
+fetchUser();
 
-        // Проверяем, что статус ответа равен 401 (Not authenticated)
-        if (response.status === 401) {
-          // Если ответ "Not authenticated", то добавляем класс "not-active" блоку
-          document.querySelector("body").classList.add("not-active");
-        } else {
-          // В противном случае добавляем класс "active"
-          document.querySelector("body").classList.add("active");
-        }
-      } catch (error) {
-        console.error("Ошибка при выполнении запроса:", error);
-      }
-    }
+const asks = Array.from(document.querySelectorAll(".section-faq__item"));
+asks.forEach((el) => {
+  el.addEventListener("click", () => {
+    const header = el.querySelector(".section-faq__ask");
+    const body = el.querySelector(".section-faq__answer");
+    const headerBox = header.getBoundingClientRect();
 
-    // Вызываем функцию проверки авторизации при загрузке страницы
-    checkAuthentication();
+    asks.forEach((ask) => {
+      if (ask == el) return;
+      const header = ask.querySelector(".section-faq__ask");
+      const box = header.getBoundingClientRect();
+      ask.classList.remove("open");
+      ask.style.height = box.height + "px";
+    });
+
+    el.style.height = headerBox.height + "px";
+    body.style.display = "block";
+    setTimeout(() => {
+      const { height } = body.getBoundingClientRect();
+      el.classList.toggle("open");
+      el.style.height = el.classList.contains("open") ? `${headerBox.height + height}px` : headerBox.height + "px";
+    }, 10);
+  });
+});
